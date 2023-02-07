3a2574b7fd555fe678ab95f666b83db372dd9f03
ItsTulasiRaman <tulasiraman917@gmail.com>
2023-02-08 02:28:46 +0530

    Added aditional screen size for responsiveness

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App({name,description}) {
 {/* Card 1 begins here */}
 <div className=''>
         <div className='flex justify-center mt-[40px] m-w-[400px]'>
-          <div className='p-[2%] w-[90%] min-w-[390px] flex md:flex-row flex-col gap-1 bdr'>
+          <div className='p-[2%] w-[90%] min-w-[350px] flex md:flex-row flex-col gap-1 bdr'>
             <div className=' w-[100%] md:h-auto xl:w-[40%] lg:w-[60%] md:w-[70%] p-[1%] '>
               <div className='card1 md:px-0 px-[20%] sm:h-[55vh] h-[220px]'>
                 <div className='fadeIn w-[85px] float-right'><img className='' src={titik} alt='white dots' /></div>
@@ -55,6 +55,45 @@ function App({name,description}) {
           </div>
         </div>
 
+
+
+
+
+
+        <div className='flex justify-center mt-[40px] m-w-[400px]'>
+          <div className='p-[2%] w-[90%] min-w-[350px] flex md:flex-row flex-col gap-1 bdr'>
+            <div className=' w-[100%] md:h-auto xl:w-[40%] lg:w-[60%] md:w-[70%] p-[1%] '>
+              <div className='card1 md:px-0 px-[20%] sm:h-[55vh] h-[220px]'>
+                <div className='fadeIn w-[85px] float-right'><img className='' src={titik} alt='white dots' /></div>
+                <div className='relative'>
+                <div className='fadeIn w-[100%] absolute flex items-end border xl:h-[45vh] lg:h-[40vh] md:h-[35vh] sm:h-[52vh] h-[220px]'><img className='w-[85px]' src={titik} alt='white dots' /></div>
+                </div>
+                <div className='relative'>
+                <div className='w-[100%] xl:top-[4vh] flex justify-center border-3 absolute lg:top-[6vh] md:top-[5vh] top-[5vh]'><img className='w-[70%]' src={artist} alt='artist' /></div>
+                </div>
+              </div>
+            </div>
+
+            <div className='md:mt-[0px] md:ml-[-20px] p-[1%]'>
+              <h1 className='text-white text-8xl'>Ritviz</h1>
+              <div className='flex flex-col gap-2 md:ml-[-40px] p-[10px]'>
+                <p className='text-white'>Lorem ipsum dolor sit amet consectetur. Et maecenas parturient vulputate netus lectus in faucibus massa dui. Varius urna pellentesque faucibus sed at. Mauris augue non imperdiet diam fermentum.</p>
+                <div className='flex flex-row gap-4'>
+                  <img className='w-[40px]' src={insta} alt='instagram' />
+                  <img className='w-[40px]' src={tweet} alt='twitter' />
+                </div>
+                <div>
+                  <button className='bg-[#E7704B] p-[10px] font-medium mb-[2%]'>
+                    <div className='flex flex-row gap-[10px]'>
+                      <p>Book tickets</p>
+                      <img src={bookticketsarrow} alt='book tickets icon' />
+                    </div>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
 
 
