796630960e7ca91ca3ded3cd6facfe1ea44b8878
ItsTulasiRaman <tulasiraman917@gmail.com>
2023-02-08 02:12:19 +0530

    Responsiveness

--- src/App.js
+++ src/App.js
@@ -22,14 +22,14 @@ function App({name,description}) {
 <div className=''>
         <div className='flex justify-center mt-[40px] m-w-[400px]'>
           <div className='p-[2%] w-[90%] flex md:flex-row flex-col gap-1 bdr'>
-            <div className='md:w-[40%] w-[100%] md:h-auto h-[50vh] p-[1%]'>
-              <div className='card1'>
-                <div className='fadeIn w-[85px] float-right '><img src={titik} alt='white dots' /></div>
-                <div className=''>
-                <div className='fadeIn w-[85px] absolute md:bottom-[20px] md:top-auto top-[33%]'><img className='' src={titik} alt='white dots' /></div>
+            <div className=' w-[100%] md:h-auto xl:w-[40%] lg:w-[60%] md:w-[70%] p-[1%] '>
+              <div className='card1 md:px-0 px-[20%] sm:h-[55vh] h-[220px]'>
+                <div className='fadeIn w-[85px] float-right'><img className='' src={titik} alt='white dots' /></div>
+                <div className='relative'>
+                <div className='fadeIn w-[100%] absolute flex items-end border xl:h-[45vh] lg:h-[40vh] md:h-[35vh] sm:h-[52vh] h-[220px]'><img className='w-[85px]' src={titik} alt='white dots' /></div>
                 </div>
                 <div className='relative'>
-                <div className='w-[75%] top-[30px] left-[10%] flex justify-center absolute'><img classname='' src={artist} alt='artist' /></div>
+                <div className='w-[100%] xl:top-[4vh] flex justify-center border-3 absolute lg:top-[6vh] md:top-[5vh] top-[5vh]'><img className='w-[70%]' src={artist} alt='artist' /></div>
                 </div>
               </div>
             </div>
